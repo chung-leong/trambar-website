bfb52182aa4fc26c3261ba0da308baad54dce65a
Chung Leong <chernyshevsky@hotmail.com>
2018-08-09 22:41:55 +0200

    Initial check-in.

---
+++ webpack.config.js
@@ -0,0 +1,93 @@
+var _ = require('lodash');
+var Path = require('path');
+var Webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var DefinePlugin = Webpack.DefinePlugin;
+var SourceMapDevToolPlugin = Webpack.SourceMapDevToolPlugin;
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+var event = process.env.npm_lifecycle_event;
+
+module.exports = {
+    context: Path.resolve('./src'),
+    entry: './main',
+    output: {
+        path: Path.resolve('./www'),
+        filename: 'app.js',
+    },
+    resolve: {
+        extensions: [ '.js', '.jsx' ],
+        modules: [ Path.resolve('./src'), Path.resolve('./node_modules') ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: [
+                        'babel-preset-es2015',
+                    ],
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: 'css-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'sass-loader',
+                    }
+                ]
+            },
+            {
+                test: /\.(jpeg|jpg|png|gif|svg)$/,
+                loader: 'file-loader',
+            },
+        ]
+    },
+    plugins: [
+        new SourceMapDevToolPlugin({
+            filename: '[file].map',
+        }),
+        new HtmlWebpackPlugin({
+            template: Path.resolve(`./src/index.html`),
+            filename: Path.resolve(`./www/index.html`),
+        }),
+    ],
+    devtool: (event === 'build') ? 'inline-source-map' : false,
+    devServer: {
+        inline: true,
+    }
+};
+
+var constants = {};
+if (event === 'build') {
+    console.log('Optimizing JS code');
+
+    // set NODE_ENV to production
+    var plugins = module.exports.plugins;
+    var constants = {
+        'process.env.NODE_ENV': '"production"',
+        'process.env.INCLUDE_DISPLAY_NAME': 'true'
+    };
+    plugins.unshift(new DefinePlugin(constants));
+
+    // use Uglify to remove dead-code
+    plugins.unshift(new UglifyJSPlugin({
+        uglifyOptions: {
+            compress: {
+              drop_console: true,
+            }
+        }
+    }));
+}
